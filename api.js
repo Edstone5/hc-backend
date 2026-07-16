d7bfee21b49ec350a43f9e1121255dbae37448d1
JheralMaquera <156086618+JheralMaquera@users.noreply.github.com>
2026-07-16 10:06:01 -0500

    fix: permitir frontend de render en cors two
    Commented out the handling of OPTIONS requests for CORS.

--- api.js
+++ api.js
@@ -35,7 +35,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // Manejar explícitamente solicitudes OPTIONS (preflight)
-app.options('*', cors(corsOptions));
+//app.options('*', cors(corsOptions));
 
 app.use(cookieParser());
 
